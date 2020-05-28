4b3b4b78b09f60c39632f70ea32548efc138216f
Alex Macdonald <almacdon@redhat.com>
2020-05-28 15:27:43 -0400

    add asciidoc formatted jfr & jmc documentation

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p docs/{getting-started,rhel,windows}
 cd books && asciidoctor index.adoc -o ../docs/index.html
 cd -
 
-for dir in getting-started rhel windows ; do
+for dir in getting-started jfr-jmc rhel windows ; do
   cd books/$dir && asciidoctor master.adoc -o ../../docs/$dir/index.html
   cd -
 done
